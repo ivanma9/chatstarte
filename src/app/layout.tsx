b8f424e96f75cba2c676bf90be72c50699da5459
Ivan Ma <43893062+ivanma9@users.noreply.github.com>
2024-11-20 19:55:30 -0800

    Enlarge body extend to the full width of screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body>
+			<body className="flex max-h">
 				<ClerkProvider dynamic>
 					<ConvexClientProvider>{children}</ConvexClientProvider>
 				</ClerkProvider>
