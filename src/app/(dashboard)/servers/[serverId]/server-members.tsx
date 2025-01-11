f762c8907b4311ad1935d554820d4854f2bdad9f
Ivan <ivanma819@gmail.com>
2025-01-10 18:13:19 -0800

    Add voice and video chat

--- src/app/(dashboard)/servers/[serverId]/server-members.tsx
+++ src/app/(dashboard)/servers/[serverId]/server-members.tsx
@@ -8,7 +8,7 @@ export default function ServerMembers({ id }: { id: Id<"servers"> }) {
 	return (
 		<div className="flex flex-col max-w-80 w-full border-l p-4 bg-muted">
 			{members?.map((member) => (
-				<div className="flex items-center gap-2" key={member._id}>
+				<div className="flex items-center gap-2 mb-2" key={member._id}>
 					<Avatar>
 						<AvatarImage src={member.image} />
 						<AvatarFallback>{member.username[0]}</AvatarFallback>
--- src/app/(dashboard)/servers/[serverId]/voice.tsx
+++ src/app/(dashboard)/servers/[serverId]/voice.tsx
@@ -10,11 +10,15 @@ import { useQuery } from "convex/react";
 import { PhoneIcon } from "lucide-react";
 import { api } from "../../../../../convex/_generated/api";
 import { Id } from "../../../../../convex/_generated/dataModel";
+import "@livekit/components-styles";
+import { useState } from "react";
+
 export default function Voice({ serverId }: { serverId: Id<"servers"> }) {
+	const [open, setOpen] = useState(false);
 	const token = useQuery(api.functions.livekit.getToken, { serverId });
 
 	return (
-		<Dialog>
+		<Dialog open={open} onOpenChange={setOpen}>
 			<DialogTrigger asChild>
 				<SidebarMenuButton>
 					<PhoneIcon className="w-4 h-4" />
@@ -26,6 +30,7 @@ export default function Voice({ serverId }: { serverId: Id<"servers"> }) {
 				<LiveKitRoom
 					serverUrl="wss://chatstarter-4abfxxpn.livekit.cloud"
 					token={token}
+					onDisconnected={() => setOpen(false)}
 				>
 					<VideoConference />
 				</LiveKitRoom>
